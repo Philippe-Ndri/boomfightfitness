6640a94db064e7a161283da2da9db2f738d8e229
Philippe-Ndri <115605147+Philippe-Ndri@users.noreply.github.com>
2022-11-01 16:54:15 +0000

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+/*const accordion = document.getElementsByClassName('contentRx');
+
+for(i = 0;<accordion.length; i++ ){
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+}*/
+
